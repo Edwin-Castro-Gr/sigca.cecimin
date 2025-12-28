8d047c9e3f5f8241a4b866444fe6885ebaec90e5
Edwin Alfonso Castro Granados <edwincas_17@hotmail.com>
2025-12-27 21:10:44 -0500

    Update login.js

--- _js/login.js
+++ _js/login.js
@@ -28,7 +28,7 @@ $(function () {
             },
             'default': { icon: 'warning', title: 'Oops...', text: 'Usuario no autorizado' }
         };
-        alert(alerts[status]);
+        alert(status);
         const alertConfig = alerts[status] || alerts['default'];
         showAlert(alertConfig.title, alertConfig.text || message, alertConfig.icon, alertConfig.action);
         if (status !== '0' && status !== '1' && status !== '5' && status !== '7') $('#usuario').focus();
