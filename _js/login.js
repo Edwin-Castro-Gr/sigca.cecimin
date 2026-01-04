fd98669a79830390aab677204f45aa9c95941ca9
Edwin-Castro-Gr <edwincas_17@hotmail.com>
2026-01-04 13:34:51 -0500

    Update login.js

--- _js/login.js
+++ _js/login.js
@@ -30,7 +30,7 @@ $(function () {
        //alert(status);
         switch (status.trim()) {
             case '0':
-                showAlert('¡Bienvenido!', message, 'success', () => {window.location.href = `/login/verify_2fa/${userId}`});
+                showAlert('¡Bienvenido!', message, 'success', () => {window.location.href = `/login/verify_2fa/${usuario}`});
                 break;
             case '1':
                 showAlert('¡Atención!', message, 'warning', () => window.open(`/login/cambiar?idreg=${usuario}`, '_parent'));
