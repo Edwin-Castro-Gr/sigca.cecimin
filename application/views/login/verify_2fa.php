1327aeb1a1e66c510b6b9d22fd5d75012120d1ae
Edwin-Castro-Gr <edwincas_17@hotmail.com>
2026-01-06 11:57:35 -0500

    Update verify_2fa.php

--- application/views/login/verify_2fa.php
+++ application/views/login/verify_2fa.php
@@ -12,7 +12,7 @@
     <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
     
     <!-- SweetAlert2 -->
-    <script src="./plugins/sweetalert2@10.16.0/dist/sweetalert2.all.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10.16.0/dist/sweetalert2.all.min.js"></script>
     
     <style>
         body {
